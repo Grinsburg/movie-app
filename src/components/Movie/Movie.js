a85038eabe1b1eff188b0c11a6dec730cc87e70f
Grinsburg <smlynchik@mail.ru>
2019-12-03 16:31:04 +0300

    upgrade MovieItem component and styles

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,16 +6,8 @@ import MovieItem from '../MovieItem';
 const Movie = () => {
   return (
     <div className={styles.container}>
-    <div class={styles.card}>
-      <img class={styles.cardImgTop} src="https://source.unsplash.com/daily?wood" alt="Card image top" />
-      <div className={styles.cardBody}>
-        <h3 className={styles.cardTitle}>Card title</h3>
-        <h4 className={styles.cardSubtitle}>Card subtitle</h4>
-        <p className={styles.cardText}>You can add navigation (navigation and pills) to card header</p>
-        <a href="?" className={styles.btn}>More</a>
-        </div>
+      <MovieItem />
     </div>
-  </div>
   );
 }
 
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import styles from './MovieItem.module.css';
+
 const MovieItem = () => {
   return (
-    <div>
-      Movie
+    <div class={styles.card}>
+      <img class={styles.cardImgTop} src="https://source.unsplash.com/daily?wood" alt="Card image top" />
+      <div className={styles.cardBody}>
+        <h3 className={styles.cardTitle}>Card title</h3>
+        <h4 className={styles.cardSubtitle}>Card subtitle</h4>
+        <p className={styles.cardText}>You can add navigation (navigation and pills) to card header</p>
+        <a href="?" className={styles.btn}>More</a>
+        </div>
     </div>
   );
 }
