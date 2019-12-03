21b10322ad03e84a3d9a2398bfd2c6baa0076991
Grinsburg <smlynchik@mail.ru>
2019-12-03 15:27:57 +0300

    add Navbar components
    q

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import styles from './Navbar.module.css'
+
 const Navbar = () => {
   return (
-    <div>
-      3
-    </div>
+    <nav className={styles.navbar}>
+      <ul>
+        <li><a href="?">Home</a></li>
+        <li><a href="?">Favorites</a></li>
+        <li><a href="?">Link</a></li>
+      </ul>
+    </nav>
   );
 }
 
