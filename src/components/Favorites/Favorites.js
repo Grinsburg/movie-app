4c5e24611c89216193ce2ce3c48e21746dc2b488
Grinsburg <smlynchik@mail.ru>
2019-12-03 17:03:50 +0300

    add FavoritesItem components and styles

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import FavoritesItem from '../FavoritesItem';
+
 import styles from './Favorites.module.css';
 
 const Favorites = () => {
   return (
     <div className={styles.container}>
-      
+      <FavoritesItem />
     </div>
   );
 }
---
+++ src/components/FavoritesItem/FavoritesItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './FavoritesItem.module.css';
+
+const FavoritesItem = () => {
+  return (
+    <div class={styles.card}>
+      <img src="https://source.unsplash.com/daily?wood" class={styles.cardImgTop} alt="Card" />
+      <div className={styles.cardBody}>
+        <h3 className={styles.cardTitle}>Card title</h3>
+        <h4 className={styles.cardSubtitle}>Card subtitle</h4>
+        <p className={styles.cardText}>You can add navigation (navigation and pills) to card header</p>
+        <a href="?" className={styles.btn}>More</a>
+        </div>
+    </div>
+  );
+}
+
+export default FavoritesItem;
