904be74eea0119c27304000f68bbcc00aa9173a7
Grinsburg <smlynchik@mail.ru>
2019-12-03 17:00:11 +0300

    add Favorites components and styles

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Favorites.module.css';
+
+const Favorites = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+}
+
+export default Favorites;
